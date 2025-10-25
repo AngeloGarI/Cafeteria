a1367b3d614b83a96a36035b8b4fb775d94d3422
Angelo <27garciaangelo@gmail.com>
2025-10-24 22:02:55 -0600

    Sigue en pruebas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from PyQt6.QtWidgets import QApplication
 from ui.login_window import LoginWindow
 from database.db_init import init_db
