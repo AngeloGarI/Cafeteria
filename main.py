0d0ef469f554f720ee527f0c35233aeb68af7950
Angelo <27garciaangelo@gmail.com>
2025-11-03 10:31:50 -0600

    Comentarios borrados

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from ui.login_window import LoginWindow
 from database.db_init import init_db
 
 if __name__ == "__main__":
-    init_db()  # Inicializa DB si no existe
+    init_db()
     app = QApplication(sys.argv)
-    with open("ui/styles.qss", "r", encoding="utf-8") as f:  # Encoding UTF-8 para caracteres Unicode
-        app.setStyleSheet(f.read())  # Carga estilos globales
+    with open("ui/styles.qss", "r", encoding="utf-8") as f:
+        app.setStyleSheet(f.read())
     window = LoginWindow()
     window.show()
     sys.exit(app.exec())
